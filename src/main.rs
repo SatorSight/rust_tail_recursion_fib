f2c341895471f90a11c57251204e9b302cb057bd
Eugene Nikolaev <eugene.nikolaev@globalsign.com>
2020-01-17 17:59:01 +0900

    init

---
+++ src/main.rs
@@ -0,0 +1,30 @@
+extern crate num_bigint as bigint;
+extern crate num_traits;
+#[macro_use] extern crate tramp;
+
+use bigint::BigUint;
+use num_traits::{Zero, One};
+use tramp::{tramp, Rec};
+
+// use bigint::{ToBigUint};
+
+fn fib(n: i32) -> BigUint {
+	// let mut acc = Zero::zero();
+	// let mut curr = One::one();
+	tramp(do_fib(n, Zero::zero(), One::one()))
+}
+
+fn do_fib(n: i32, acc: BigUint, curr: BigUint) -> Rec<BigUint> {
+	if n <= 0 {
+		rec_ret!(acc)
+	}else{
+		let new = &acc + curr;
+		let nn = n - 1;
+		rec_call!(do_fib(nn, new, acc))
+	}
+}
+
+fn main() {
+	let n = 1000000;
+	println!("{}", fib(n));
+}
